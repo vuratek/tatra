a81348d7355645d906c73042e3ae9cb467708e7c
Otmar Olsina <otmar.olsina@nasa.gov>
2023-02-27 11:51:38 -0500

    carousel fix

--- src/tatra/carousel/Data.ts
+++ src/tatra/carousel/Data.ts
@@ -2,6 +2,7 @@ export class Image {
     public image        : string = '';
     public label        : string | null = null;
     public date         : string | null = null;
+    public offsetY      : string | null = null;
     public url          : string | null = null;
 }
 
